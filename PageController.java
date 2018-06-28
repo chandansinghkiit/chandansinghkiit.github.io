fffd1ee3db56109f95e41d57d31b68176194a48a
chandan singh <chandansinghkiit@gmail.com>
2018-06-28 10:50:22 +0530

    Exception handling
    exception concept

--- PageController.java
+++ PageController.java
@@ -129,3 +129,163 @@ public class PageController {
 	}
 	
 }
+Global error handling 
+=============================================================
+package com.mystyle.onlineshopping.exception;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalDefaultExceptionHandler {
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ModelAndView handlerNoHandlerFoundException()
+	{
+		ModelAndView mv= new ModelAndView("error");
+		
+		mv.addObject("errorTitle","Page not constructed !!!!!");
+        mv.addObject("errorDescription","The page you looking not available !!!!!");
+
+		mv.addObject("title"," 404 Error page");
+		return mv;
+		
+	}
+	
+	@ExceptionHandler(ProductNotFoundException.class)
+	public ModelAndView handleProductNotFoundException()
+	{
+		ModelAndView mv= new ModelAndView("error");
+		
+		mv.addObject("errorTitle","Product not available !!!!!");
+        mv.addObject("errorDescription","The Product you looking not available right now!!!!!");
+
+		mv.addObject("title","Product unavailble");
+		return mv;
+		
+	}
+	
+	
+	@ExceptionHandler(Exception.class)
+	public ModelAndView handleNotFoundException(Exception ex)
+	{
+		ModelAndView mv= new ModelAndView("error");
+		
+		mv.addObject("errorTitle","Contact your Administrator !!!!!");
+		
+		//testing 
+		StringWriter sw=new StringWriter();
+		PrintWriter pw=new PrintWriter(sw);
+		
+		ex.printStackTrace(pw);
+		
+        mv.addObject("errorDescription",sw.toString());
+
+		mv.addObject("title","Error");
+		return mv;
+		
+	}
+	
+	
+
+}
+
+
+
+=============================================================
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@taglib prefix="spring" uri="http://www.springframework.org/tags"%>
+
+<spring:url var="css" value="/resources/css" />
+
+<c:set var="contextRoot" value="${pageContext.request.contextPath}" />
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<!-- Bootstrap Core CSS -->
+<link href="${css}/bootstrap.min.css" rel="stylesheet">
+
+<!-- Bootstrap Readable Theme -->
+<link href="${css}/bootstrap-readable-theme.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="${css}/myapp.css" rel="stylesheet">
+
+
+<title>Online Shopping - ${title}</title>
+
+</head>
+
+<body>
+
+	<div class="wrapper">
+
+	    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+	        <div class="container">
+	            <!-- Brand and toggle get grouped for better mobile display -->
+	            <div class="navbar-header">
+	                <a class="navbar-brand" href="${contextRoot}/home">Home</a>
+	            </div>
+			</div>
+		</nav>
+			
+		
+		<div class="content">
+		
+			<div class="container">
+			
+				<div class="row">
+				
+					<div class="col-xs-12">
+					
+						
+						<div class="jumbotron">
+						
+							<h1>${errorTitle}</h1>
+							<hr/>
+						
+							<blockquote style="word-wrap:break-word">
+								
+								${errorDescription}
+							
+							</blockquote>						
+						
+						</div>
+						
+											
+					</div>					
+				
+				</div>
+			
+			</div>
+							
+		</div>
+
+		
+		<%@include file="./shared/footer.jsp" %>
+
+	</div>
+		
+	
+</body>
+
+
+</html>
+
+======================================================================
